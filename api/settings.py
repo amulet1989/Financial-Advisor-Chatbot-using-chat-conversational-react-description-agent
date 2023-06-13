0b15864b200302dd67eec210a3c480fba68b63bf
amulet1989 <muletdelosreyes89@gmail.com>
2023-06-13 10:37:27 -0300

    final changes

---
+++ api/settings.py
@@ -0,0 +1,17 @@
+import os
+
+# Run API in Debug mode
+API_DEBUG = True
+
+# REDIS settings
+# Queue name
+REDIS_QUEUE = "service_queue"
+# Port
+REDIS_PORT = 6379
+# DB Id
+REDIS_DB_ID = 0
+# Host IP
+REDIS_IP = os.getenv("REDIS_IP", "redis")
+# Sleep parameters which manages the
+# interval between requests to our redis queue
+API_SLEEP = 0.05
---
+++ api/views.py
@@ -0,0 +1,33 @@
+import logging
+from uuid import uuid4
+from flask import Blueprint, request, abort
+from middleware import model_predict
+
+router = Blueprint("app_router", __name__)
+
+router.register_error_handler
+
+
+@router.route("/")
+def index():
+    return "hello world"
+
+
+@router.route("/api/ask_model", methods=["POST"])
+def ask_model():
+    """
+    This function is used to get the response from the agent model
+    """
+    try:
+        messages = request.json["messages"]
+        user = str(uuid4())
+
+        ## Validation to get the last 8 messages to the conversation
+        if len(messages) > 8:
+            messages = messages[-8:]
+        response = model_predict(messages, user)
+        return {"user": user, "content": response}
+    except Exception as e:
+        error_message = str(e)
+        logging.error(error_message)
+        abort(500, description=error_message)
